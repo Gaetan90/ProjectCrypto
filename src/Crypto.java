298605891b7a542cd60ff934fbb226d50941d60d
Gaetan90 <gaetan.lopezcutillas@viacesi.fr>
2018-04-08 14:37:19 +0200

    First

---
+++ src/Crypto.java
@@ -0,0 +1,86 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
+
+public class Crypto {
+
+//    private static String encode_using_xor(final byte[] input, final byte[] secret) {
+//        final byte[] output = new byte[input.length];
+//        if (secret.length == 0) {
+//            throw new IllegalArgumentException("empty security key");
+//        }
+//        int spos = 0;
+//        for (int pos = 0; pos < input.length; ++pos) {
+//            output[pos] = (byte) (input[pos] ^ secret[spos]);
+//            ++spos;
+//            if (spos >= secret.length) {
+//                spos = 0;
+//            }
+//        }
+//        return new sun.misc.BASE64Encoder().encode(output);
+//    }
+
+    public static String decode_operation(String s,final byte[] key){
+        int spos = 0;
+        try{
+            byte [] output= s.getBytes("US-ASCII");
+            for (int pos = 0; pos < output.length; ++pos) {
+                output[pos] = (byte) (output[pos] ^ key[spos]);
+                ++spos;
+                if (spos >= key.length) {
+                    spos = 0;
+                }
+            }
+            return new String(output, "UTF-8");
+        }catch(Exception e){
+
+        }
+        return null;
+    }
+
+//    private static String decode_operation(String s,final byte[] key){
+//        int spos = 0;
+//        try{
+//            byte [] output=(new sun.misc.BASE64Decoder().decodeBuffer(s));
+//            for (int pos = 0; pos < output.length; ++pos) {
+//                output[pos] = (byte) (output[pos] ^ key[spos]);
+//                ++spos;
+//                if (spos >= key.length) {
+//                    spos = 0;
+//                }
+//            }
+//            return new String(output, "UTF-8");
+//        }catch(Exception e){
+//
+//        }
+//        return null;
+//    }
+}
+//        public static void main(String[] args) {
+//            try{
+//                String orig = "encode me £µé#^^]@12&~#{[,?-yassinefadhlaoui-engeneer";
+//                String s="Iw8HBwgETxgMEvGW9d6m0EUxLAUPQ1dIHVEiK1hWQhcnEhcBAgQJFA1aX1RYHgxUAwEVPSEXABw=";
+//                String key="Fadhlaoui2357keyforXOrencrYption";
+//                byte [] input= orig.getBytes("UTF-8");
+//                byte [] keyb=key.getBytes();
+//                System.out.println("Original String : "+orig);
+//                System.out.println("String after encrytion :  "+encode_using_xor(input,keyb));
+//                System.out.println("String after decrytion :  "+decode_operation(s,keyb));
+//            }catch(Exception e){
+//
+//            }
+//
+//
+//        }
+
+
+/*output :
+ * Original String : encode me £µé#^^]@12&~#{[,?-yassinefadhlaoui-engeneer
+String after encrytion :  Iw8HBwgETxgMEvGW9d6m0EUxLAUPQ1dIHVEiK1hWQhcnEhcBAgQJFA1aX1RYHgxUAwEVPSEXABw=
+String after decrytion :  encode me £µé#^^]@12&~#{[,?-yassinefadhlaoui-engeneer
+*/
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,85 @@
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class Main {
+
+    private static Charset USASCII = Charset.forName("UTF-8");
+    private static char charAlphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+
+        Crypto crypto = new Crypto();
+        Scanner s = new Scanner(System.in);
+        String line = "";
+        File file = new File("C:/Users/Gaetan/Documents/Fichiers/PD.txt");
+        Charset charset = Charset.forName("Cp1252");
+        String key= "";
+        String result= "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:/Users/Gaetan/Documents/Fichiers/PG.txt"), USASCII))) {
+            do{
+                //if(reader.readLine()!="\n")
+                    line += reader.readLine();
+                    line += " ";
+            }while(reader.readLine()!=null);
+
+            key = "vrhpaf";
+
+            result = crypto.decode_operation( line, key.getBytes("US-ASCII"));
+            System.out.println(key +"  ----  "+ result);
+            System.out.println(line.length());
+
+
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+
+//        int time =0;
+//        Scanner scanner = new Scanner(file);
+//
+//        String message = "Fx8JCRsdHw==";
+//
+//        String result = "";
+//        for(int a1 =0;a1<26;a1++){
+//            for(int a2 =0;a2<26;a2++){
+//                for(int a3 =0;a3<26;a3++){
+//                    for(int a4 =0;a4<26;a4++){
+//                        for(int a5 =0;a5<26;a5++){
+//                            for(int a6 =0;a6<26;a6++){
+//                                key += charAlphabet[a1];
+//                                key +=charAlphabet[a2];
+//                                key +=charAlphabet[a3];
+//                                key +=charAlphabet[a4];
+//                                key +=charAlphabet[a5];
+//                                key +=charAlphabet[a6];
+//                                //System.out.println(crypto.decode_operation(message, key));
+//
+//                                result = crypto.decode_operation( line, "vrhpaf");
+//                                if(result!= null){
+//                                    System.out.println(key +"  ----  "+ result);
+//                                    System.out.println(time);
+//                                    System.exit(0);
+//                                }
+//
+//
+//                                time++;
+//                                key="";
+//                            }
+//
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+
+    }
+
+}
